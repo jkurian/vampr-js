90912908c5fa012cda2a220b44af2b43ca38a842
Jerry Kurian <jkurian770@gmail.com>
2018-02-13 10:19:23 -0500

    Vampire tree implementation

--- vampire.js
+++ vampire.js
@@ -10,22 +10,31 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length; //or is this supposed to include children of children?
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let distance = 0;
+    let currentVampire = this;
+    
+    while(currentVampire.creator) {
+      distance++;
+      currentVampire = currentVampire.creator;
+    }
+    return distance;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    if(vampire.numberOfVampiresFromOriginal > this.numberOfVampiresFromOriginal) return true
+    return false
   }
 
   /** Stretch **/
@@ -36,7 +45,7 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    
   }
 }
 
